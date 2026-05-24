8cbff27b625d6d908301fc2800704c9b10c7f786
xand1ex999 <yojiik43@gmail.com>
2026-05-24 23:30:42 +0100

    feature/added page to see backend server status

--- src/main/java/com/io/banking/shared/health/HealthController.java
+++ src/main/java/com/io/banking/shared/health/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/")
+    @GetMapping("/health")
     public String healthCheck() {
         return "Server is up and running successfully!";
     }
