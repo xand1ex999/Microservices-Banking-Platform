7c4225e96ae8434f0a9d6c62b962f8c90c971b1f
xand1ex999 <yojiik43@gmail.com>
2026-05-27 00:24:41 +0100

    chore: wire root .env into monolith docker service

--- notification-service/src/main/java/com/io/banking/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/io/banking/notification/NotificationServiceApplication.java
@@ -3,10 +3,16 @@ package com.io.banking.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
+
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(NotificationServiceApplication.class, args);
+        System.out.println("Notification service started");
+
     }
 }
